8875b6d4ea7eced0507d0a07767ee23397e74c79
fsampson30 <fsampson30@gmail.com>
2023-11-30 07:39:21 -0300

    Created first controller class.

---
+++ src/main/java/com/sampson/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,30 @@
+package com.sampson.restfulwebservices.helloworld;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorldBean {
+    private String message;
+
+    public HelloWorldBean() {
+    }
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}

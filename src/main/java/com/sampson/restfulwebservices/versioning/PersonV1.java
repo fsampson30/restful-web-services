bd21921f4e62c03b86b17665a179b74f6cb4d56e
fsampson30 <fsampson30@gmail.com>
2023-12-14 07:26:10 -0300

    Create endpoint to test versioning.

---
+++ src/main/java/com/sampson/restfulwebservices/versioning/PersonV1.java
@@ -0,0 +1,27 @@
+package com.sampson.restfulwebservices.versioning;
+
+public class PersonV1 {
+
+    private String name;
+    public PersonV1(String name) {
+        this.name = name;
+    }
+
+    public PersonV1() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonV1{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}

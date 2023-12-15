b71499dd61d05a1d0d3a5fec08c86cbc8c86f4d9
fsampson30 <fsampson30@gmail.com>
2023-12-15 07:18:32 -0300

    Create endpoint to return Person using request header.

--- src/main/java/com/sampson/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/com/sampson/restfulwebservices/versioning/VersioningPersonController.java
@@ -26,4 +26,14 @@ public class VersioningPersonController {
         return new PersonV2(new Name("Bob", "Charlie"));
     }
 
+    @GetMapping(path = "person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeaders(){
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeaders(){
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
 }

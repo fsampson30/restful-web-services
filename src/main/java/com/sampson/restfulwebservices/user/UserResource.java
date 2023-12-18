f7bb2b638a217b83c5ce0aaa4b1c1bf3b9b23d6d
fsampson30 <fsampson30@gmail.com>
2023-12-18 07:24:51 -0300

    Refactored the endpoint to return a link in the json.

--- src/main/java/com/sampson/restfulwebservices/user/UserResource.java
+++ src/main/java/com/sampson/restfulwebservices/user/UserResource.java
@@ -1,6 +1,8 @@
 package com.sampson.restfulwebservices.user;
 
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -23,12 +25,17 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id){
+    public EntityModel<User> retrieveUser(@PathVariable int id){
         User user = userDaoService.findOne(id);
         if (user==null){
             throw new UserNotFoundException("id:"+id);
         }
-        return user;
+
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder linkBuilder = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(linkBuilder.withRel("all-users"));
+
+        return entityModel;
     }
 
     @DeleteMapping("/users/{id}")

5c3ae02c6a0e8c1117ffc37df5e593e2092bcbf6
fsampson30 <fsampson30@gmail.com>
2023-12-20 07:12:03 -0300

    Created new unit tests to User class.

--- src/test/java/com/sampson/restfulwebservices/user/UserTest.java
+++ src/test/java/com/sampson/restfulwebservices/user/UserTest.java
@@ -1,20 +1,39 @@
 package com.sampson.restfulwebservices.user;
 
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class UserTest {
+
+    User user;
+
+    @BeforeEach
+    public void createUser(){
+        user = new User(1,"Flavio", LocalDate.of(2000,1,1));
+    }
+
 
     @Test
     public void validateTypes(){
-        User user = new User(1,"Flavio", LocalDate.of(2000,1,1));
         Assertions.assertInstanceOf(Integer.class,user.getId());
         Assertions.assertInstanceOf(String.class, user.getName());
         Assertions.assertInstanceOf(LocalDate.class, user.getBirthDate());
     }
+
+    @Test
+    public void testNameSize(){
+        Assertions.assertNotEquals(0,user.getName().length());
+    }
+
+    @Test
+    public void testBirthDate(){
+        Assertions.assertTrue(user.getBirthDate().isBefore(LocalDate.now()));
+    }
+
+
 
 }

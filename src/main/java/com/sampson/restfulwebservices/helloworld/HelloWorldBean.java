bf5673d70dab3b7e796529e2c8aa5b3b9b3784ac
fsampson30 <fsampson30@gmail.com>
2024-01-09 08:02:02 -0300

    Created validation.

--- src/main/java/com/sampson/restfulwebservices/helloworld/HelloWorldBean.java
+++ src/main/java/com/sampson/restfulwebservices/helloworld/HelloWorldBean.java
@@ -1,9 +1,11 @@
 package com.sampson.restfulwebservices.helloworld;
 
+import jakarta.validation.constraints.Size;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HelloWorldBean {
+    @Size(min = 10)
     private String message;
 
     public HelloWorldBean() {

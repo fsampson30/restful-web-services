c972c3ab0a60c5cd19a3d5748d08e2b37f182bae
fsampson30 <fsampson30@gmail.com>
2024-02-20 08:00:12 -0300

    Added JWT classes.

---
+++ src/main/java/com/sampson/restfulwebservices/jwt/JwtTokenRequest.java
@@ -0,0 +1,3 @@
+package com.sampson.restfulwebservices.jwt;
+
+public record JwtTokenRequest(String username, String password) {}
---
+++ src/main/java/com/sampson/restfulwebservices/jwt/JwtTokenResponse.java
@@ -0,0 +1,3 @@
+package com.sampson.restfulwebservices.jwt;
+
+public record JwtTokenResponse(String token) {}
